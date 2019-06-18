f8f9c482ce8cded89631f8b8164eaaf0ad9cfc8c
geapsapp <geapsapp@gmail.com>
2019-06-18 16:19:46 +0100

    bugfix

--- sample/src/main/java/com/admanager/sample/activity/Splash1Activity.java
+++ sample/src/main/java/com/admanager/sample/activity/Splash1Activity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.admanager.admob.AdmobAdapter;
 import com.admanager.config.RemoteConfigHelper;
 import com.admanager.core.AdManagerBuilder;
-import com.admanager.core.DummyAdapter;
+import com.admanager.facebook.FacebookAdapter;
+import com.admanager.sample.RCUtils;
 
 /**
  * Created by Gust on 20.11.2018.
@@ -22,11 +24,9 @@ public class Splash1Activity extends AppCompatActivity {
         //        RemoteConfigHelper.setAdsEnabled(!BuildConfig.DEBUG);
 
         new AdManagerBuilder(this)
-                .add(new DummyAdapter())
-                .thenStart(SampleWAStickersActivity.class)
-//                .add(new AdmobAdapter(RCUtils.S1_ADMOB_ENABLED).withRemoteConfigId(RCUtils.S1_ADMOB_ID))
-//                .add(new FacebookAdapter(RCUtils.S1_FACEBOOK_ENABLED).withRemoteConfigId(RCUtils.S1_FACEBOOK_ID))
-//                .thenStart(Splash2Activity.class)
+                .add(new AdmobAdapter(RCUtils.S1_ADMOB_ENABLED).withRemoteConfigId(RCUtils.S1_ADMOB_ID))
+                .add(new FacebookAdapter(RCUtils.S1_FACEBOOK_ENABLED).withRemoteConfigId(RCUtils.S1_FACEBOOK_ID))
+                .thenStart(Splash2Activity.class)
                 .build()
                 .show();
 
